672dabf51f765a8465e124949ee765d90a2e897e
Eduards Janītis (221RDB271) <118062580+EduardsJ1@users.noreply.github.com>
2023-03-15 23:44:52 +0000

    commit

--- main.py
+++ main.py
@@ -2,8 +2,17 @@
 
 def parallel_processing(n, m, data):
     output = []
+    ft=[0]*n
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
+    for i in range(m):
+        t=0
+        for j in range(n):
+            if ft[j]< ft[t]:
+                t=j
+        output.append((t,ft[t]))
+        ft[t]= data[i]+ft[t]
+        
 
     return output
 
@@ -13,17 +22,21 @@ def main():
     # first line - n and m
     # n - thread count 
     # m - job count
-    n = 0
-    m = 0
+    n,m = map(int, input().split())
+    #print(n)
+    #print(m)
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
+    data = list(map(int,input().split()))
+    #print(data)
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
     
     # TODO: print out the results, each pair in it's own line
+    for i,j in result:
+        print(i,j)
 
 
 
